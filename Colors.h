b61d19cdc30adf94071b26ff972178edbcfb3869
Kisara05 <139782205+Kisara05@users.noreply.github.com>
2024-10-26 07:17:14 +0700

    Add files via upload

---
+++ Colors.h
@@ -0,0 +1,14 @@
+/*#pragma once
+#include <raylib.h>
+#include <vector>
+
+extern const Color DARKGRAY;
+extern const Color GREEN;
+extern const Color RED;
+extern const Color ORANGE;
+extern const Color YELLOW;
+extern const Color PURPLE;
+extern const Color BLUE;
+extern const Color DARKBLUE;
+
+std::vector<Color> GetFiguresColors();*/
