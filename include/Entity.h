2ff01fa5522adc1f58b9d221b247f067b03132c4
Chris-KH <klowgamervn@gmail.com>
2024-11-14 12:40:39 +0700

    move feature for entity..

--- include/Entity.h
+++ include/Entity.h
@@ -1,6 +1,26 @@
-#pragma once
+﻿#pragma once
+#include <raylib.h>
 
 class Entity {
+private:
+    Vector2 position;
+    Vector2 size;
+    Texture2D texture;
+    Rectangle sourceRect;
+    Vector2 velocity;
+    float speed;
+    bool isFlipped;
 
+public:
+    // Constructor
+    Entity(Vector2 pos, Vector2 sz, Texture2D tex, float spd) : position(pos), size(sz), texture(tex), velocity{ 0, 0 }, speed(spd), isFlipped(false) {
+        sourceRect = { 0, 0, (float)tex.width, (float)tex.height };
+    }
+
+    Rectangle getHitBox();
+    void setPosition(Vector2 newPos);
+    void handleInput();
+    void move();
+    void draw() const;
+    void unloadTexture() const;
 };
-
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,48 @@
-#include "../include/Entity.h"
+﻿#include "../include/Entity.h"
+
+Rectangle Entity::getHitBox() {
+    return Rectangle(position.x, position.y, size.x, size.y);
+}
+
+void Entity::setPosition(Vector2 newPos) { position = newPos; }
+
+void Entity::handleInput() {
+    velocity = { 0, 0 };
+    if (IsKeyDown(KEY_RIGHT)) velocity.x += speed;
+    if (IsKeyDown(KEY_LEFT)) velocity.x -= speed;
+    if (IsKeyDown(KEY_DOWN)) velocity.y += speed;
+    if (IsKeyDown(KEY_UP)) velocity.y -= speed;
+
+    if (IsKeyDown(KEY_D)) velocity.x += speed;
+    if (IsKeyDown(KEY_A)) velocity.x -= speed;
+    if (IsKeyDown(KEY_S)) velocity.y += speed;
+    if (IsKeyDown(KEY_W)) velocity.y -= speed;
+
+    if (velocity.x < 0) {
+        isFlipped = true;  // Flip 180 degree to the left
+    }
+    else if (velocity.x > 0) {
+        isFlipped = false;
+    }
+}
+
+void Entity::move() {
+    float deltaTime = GetFrameTime();
+    position.x += velocity.x * deltaTime;
+    position.y += velocity.y * deltaTime;
+}
+
+void Entity::draw() const {
+    Vector2 origin = { size.x / 2, size.y / 2 };
+
+    // Flip sourceRect width to mirror the image when isFlipped is true
+    Rectangle flippedSourceRect = sourceRect;
+    flippedSourceRect.width = isFlipped ? -sourceRect.width : sourceRect.width;
+
+    Rectangle destRect = { position.x, position.y, size.x, size.y };
+    DrawTexturePro(texture, flippedSourceRect, destRect, origin, 0.0f, WHITE);
+}
+
+void Entity::unloadTexture() const {
+    UnloadTexture(texture);
+}
