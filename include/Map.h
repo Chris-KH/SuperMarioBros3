ac9c8ac26ad2b51b9aa81922eabf339c14f1de3e
NghiaLe4575 <160830150+NghiaLe4575@users.noreply.github.com>
2024-11-14 13:28:10 +0700

    meger

---
+++ include/Map.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "Entity.h"
+#include "Item.h"
+#include "Enemy.h"
+#include "Hitbox.h"
+#include <vector>
+
+class Map {
+public:
+    Map();
+
+    ~Map();
+    virtual void fluff() = 0;
+    virtual void background() = 0;
+
+};
+
+class Block : public Map {
+public:
+    Block(int x, int y, int width, int height) {
+        hitbox.setPosition(x, y);
+        hitbox.setSize(width, height);
+    }
+    /*void render() {
+
+    }
+    void update() {
+
+    }*/
+    Hitbox getHitbox() const {
+        return hitbox;
+    }
+
+private:
+    Hitbox hitbox;
+    int x, y;
+};
+
+class InteractBlock : public Block {
+protected:
+    std::vector<Block*> block;
+    std::vector<Item*> item;
+    std::vector<Enemy*> enemy;
+
+public:
+
+};
+
+class NonInteractBlock : public Block {
+
+};
+
+class NonInteractMoveBlock : public NonInteractBlock {
+
+};
+
+class Lava : public Map {
+public:
+    Lava(int x, int y, int width, int height) {
+        hitbox.setPosition(x, y);
+        hitbox.setSize(width, height);
+    }
+    /*void render() {
+
+    }
+    void update() {
+
+    }*/
+    Hitbox getHitbox() const {
+        return hitbox;
+    }
+
+private:
+    Hitbox hitbox;
+    int x, y;
+
+};
+
+class Pipe : public Map {
+    Pipe(int x, int y, int width, int height) {
+        hitbox.setPosition(x, y);
+        hitbox.setSize(width, height);
+    }
+    /*void render() {
+
+    }
+    void update() {
+
+    }*/
+    Hitbox getHitbox() const {
+        return hitbox;
+    }
+
+private:
+    Hitbox hitbox;
+    int x, y;
+
+};
