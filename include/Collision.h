f6a2bf9d499770612ee31fad4e091eeab3a1c28f
NghiaLe4575 <160830150+NghiaLe4575@users.noreply.github.com>
2024-11-21 13:25:59 +0700

    change name of strategy to collision

--- include/Collision.h
+++ include/Collision.h
@@ -1,5 +1,20 @@
 #pragma once
 
-class Collision {
-    
+//#include"../include/"
+class Entity;
+class Block;
+
+class ICollisionStrategy {
+public:
+    virtual bool collides(Entity& entity, const Block& block) = 0;
+    virtual ~ICollisionStrategy() = default;
+};
+class EntityToBlockPushbackCollision : public ICollisionStrategy {
+public:
+    bool collides(Entity& entity, const Block& block) override;
+};
+class EntityToBLockCollision : public ICollisionStrategy
+{
+public:
+    bool collides(Entity& entity, const Block& block) override;
 };
