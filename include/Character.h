32d1e44e5e3538682df421acfdcce837fa3a9cba
Chris-KH <klowgamervn@gmail.com>
2024-11-28 14:50:58 +0700

    Create State...

--- include/Character.h
+++ include/Character.h
@@ -1,12 +1,28 @@
 #pragma once
-
+#include "Animation.h"
 #include "Sprite.h"
 #include "Hitbox.h"
 
 class Character : public Sprite {
-private:
+protected:
+    Animation runAnimation;
+    Animation jumpAnimation;
+    Animation idleAnimation;
+    Animation* currentAnimation;
 
 public:
+    Character(Vector2 pos = { 0, 0 }, Vector2 size = { 1, 1 }, Color col = WHITE);
 
+    // Override draw to use animations
+    void draw() const;
+
+    // Method to handle movement (keyboard input)
+    void move(float deltaTime);
+
+    // Method to handle jumping
+    void jump();
+
+    // Method to switch between animations
+    void switchAnimation(Animation& animation);
 };
 
--- include/Enemy.h
+++ include/Enemy.h
@@ -1,8 +1,28 @@
 #pragma once
-#include"Sprite.h"
+#include "Sprite.h"
+#include "Animation.h"
 
 class Enemy : public Sprite {
-private:
-		
+protected:
+    Animation walkAnimation;
+    Animation attackAnimation;
+    Animation idleAnimation;
+    Animation jumpAnimation;
+    Animation* currentAnimation;
+
+    bool isJumping;
+    bool isAttacking;
+    bool isWalking;
+
 public:
+    Enemy(Vector2 pos = { 0, 0 }, Vector2 size = { 1, 1 }, Color col = WHITE);
+
+    // Override draw to use animations
+    void draw() const;
+
+    // Method to move enemy (AI-controlled)
+    void move(float deltaTime);
+
+    // Method to switch between animations
+    void switchAnimation(Animation& animation);
 };
