0137111a8089e2d4fc31dff23448170d7b382b52
BlueAngel05 <hoangdonthienhoa@gmail.com>
2024-12-18 23:35:24 +0700

    fix Menu

--- include/Menu.h
+++ include/Menu.h
@@ -18,7 +18,7 @@ public:
 
     void configureSettings(bool audioEnabled, bool musicEnabled);
     void selectCharacter(int characterIndex);
-    // void selectMap(int mapIndex);
+    void selectMap(int mapIndex);
 
     // Accessors for settings
     bool isAudioEnabled() const;
@@ -28,16 +28,18 @@ public:
 
     // State management
     void returnToMainMenu();
+
     Character* player;
     Map* map;
+
 private:
     bool audioEnabled;
     bool musicEnabled;
     int selectedCharacter;
     int selectedMap;
     std::unique_ptr<MenuState> currentState;
+    Texture2D titleScreen;
 };
-
 
 // Base MenuState class
 class MenuState {
@@ -58,6 +60,11 @@ public:
     explicit MainMenuState(Menu* menu); // Constructor declaration
     void draw() override;
     void handleInput() override;
+private:
+    Rectangle startButton{300, 200, 200, 40};
+    Rectangle settingsButton{300, 250, 200, 40};
+    Rectangle charSelectionButton{300, 300, 200, 40};
+    Rectangle mapSelectionButton{300, 350, 200, 40};
 };
 
 class SettingState : public MenuState {
@@ -65,6 +72,10 @@ public:
     explicit SettingState(Menu* menu);
     void draw() override;
     void handleInput() override;
+private:
+    Rectangle audioButton{300, 300, 200, 40};
+    Rectangle musicButton{300, 350, 200, 40};
+    Rectangle backButton{300, 400, 200, 40};
 };
 
 class CharSelection : public MenuState {
@@ -72,6 +83,10 @@ public:
     explicit CharSelection(Menu* menu);
     void draw() override;
     void handleInput() override;
+private:
+    Rectangle marioButton{300, 200, 200, 40};
+    Rectangle luigiButton{300, 250, 200, 40};
+    Rectangle backButton{300, 300, 200, 40};
 };
 
 // MapState
@@ -80,4 +95,9 @@ public:
     explicit MapSelection(Menu* menu);
     void draw() override;
     void handleInput() override;
+private:
+    Rectangle map1Button{300, 200, 200, 40};
+    Rectangle map2Button{300, 250, 200, 40};
+    Rectangle map3Button{300, 300, 200, 40};
+    Rectangle backButton{300, 350, 200, 40};
 };
