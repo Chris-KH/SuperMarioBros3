5ebb01645a2b799ae3876ae806715dac9221bc5d
BlueAngel05 <hoangdonthienhoa@gmail.com>
2024-12-25 09:58:36 +0700

    final fix

--- include/Entity.h
+++ include/Entity.h
@@ -10,12 +10,13 @@ class Entity {
 private:
     Vector2 position; // Position of the entity
     Vector2 size;     // Size of the entity
-    bool dead;           // Indicates if the sprite is "dead"
+    Color color;      // RGBA color of the entity
+    bool dead;        // Indicates if the sprite is "dead"
 protected:
     Animation* currentAnimation;
     float playTime;
 public:
-    Entity(Vector2 pos = { 0, 0 }, Vector2 size = { 1, 1 });
+    Entity(Vector2 pos = { 0, 0 }, Vector2 size = { 1, 1 }, Color color = WHITE);
     Entity(const Entity& other);
 
     virtual ~Entity();
@@ -26,8 +27,9 @@ public:
     virtual float getWidth() const;
     virtual float getHeight() const;
     virtual Rectangle getRectangle() const;
-    virtual const Vector2& getSize() const ;
-    virtual const Vector2& getPosition() const;
+    virtual Vector2 getSize() const ;
+    virtual Vector2 getPosition() const;
+    virtual Color getColor() const;
     virtual EntityType getType() const = 0;
     virtual float getCenterX() const;
     virtual float getCenterY() const;
@@ -42,6 +44,7 @@ public:
     virtual void setXPosition(float x);
     virtual void setYPosition(float y);
     virtual void setSize(Vector2 sz);
+    virtual void setColor(Color col);
     virtual void setCenter(Vector2 center);
     
     // Pure virtual draw function
